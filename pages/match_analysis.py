f7eea1d59303b9f26db607d58bfcc6d63a99e5b9
Saulo Faria <saulo82faria@gmail.com>
2026-02-12 09:50:20 -0300

    Update match_analysis.py

--- pages/match_analysis.py
+++ pages/match_analysis.py
@@ -132,7 +132,7 @@ if submitted:
             with col3:
                 st.image(f"{match['teams']['away']['image']}", use_container_width=True)
 
-            st.dataframe(df_shots, ignore_index=True)
+            st.dataframe(df_shots, hide_index=True)
 
             fig = px.scatter(data_frame=df_shots, x='min', y='xg', color='team', color_discrete_sequence=[color_home, color_away], size='size', symbol='outcome', 
                                     hover_name='team', hover_data={'xgot': True, 'team': False, 'player': True, 'type': True, 'situation': True, 'outcome': True, 'size': False},  
