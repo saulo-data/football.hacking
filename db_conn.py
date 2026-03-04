f5a54c4c77264e15e9ab9c4510a4ac5cf57a7ecd
Saulo Faria <saulo82faria@gmail.com>
2026-03-03 22:07:41 -0300

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -3,3 +3,7 @@ import streamlit as st
 
 client = MongoClient(st.secrets['url_con'])
 db = client.football_data
+
+client2 = MongoClient(st.secrets['url_board'])
+db_local = client2.football_data
+col_users = db_local.users
