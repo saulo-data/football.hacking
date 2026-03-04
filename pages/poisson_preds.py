7b47a70c810e79154a8e78bfc0570ffcc00fff60
Saulo Faria <saulo82faria@gmail.com>
2026-03-04 17:53:18 -0300

    Update poisson_preds.py

--- pages/poisson_preds.py
+++ pages/poisson_preds.py
@@ -541,7 +541,7 @@ if st.session_state['logged_in']:
             try:
                 plot_venue_performances(df=df_league, home_team=home, away_team=away)
             except Exception:
-                st.image('../static/sorry.png')
+                st.info("Sorry, but there is no advanced data available for this league.")
     
         st.divider()
     
